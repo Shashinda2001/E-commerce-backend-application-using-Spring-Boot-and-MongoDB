144fb49b08cf68e392c3b5c4a13d07bff1b94541
Shashinda2001 <142221873+Shashinda2001@users.noreply.github.com>
2024-07-08 17:29:17 +0530

    create model Product

---
+++ src/main/java/com/example/Ecommerce_backend/model/Product.java
@@ -0,0 +1,68 @@
+package com.example.Ecommerce_backend.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "products")
+public class Product {
+
+    @Id
+    private String id;
+    private String name;
+    private String shortDescription;
+    private String longDescription;
+    private double price;
+
+    public Product() {
+        super();
+    }
+
+    public Product(String name, String shortDescription, String longDescription, double price) {
+        this.name = name;
+        this.shortDescription = shortDescription;
+        this.longDescription = longDescription;
+        this.price = price;
+    }
+
+    // Getters and Setters
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
