80a7f95eebfdcc8db95af04265c1202725033f6c
Shashinda2001 <142221873+Shashinda2001@users.noreply.github.com>
2024-07-09 18:18:41 +0530

    create inventory model

---
+++ src/main/java/com/example/Ecommerce_backend/model/Inventory.java
@@ -0,0 +1,51 @@
+package com.example.Ecommerce_backend.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "inventory")
+public class Inventory {
+
+    @Id
+    private String id;
+
+    @DBRef
+    private Product product;
+
+    private int quantity;
+
+    // Constructors, getters, and setters
+
+    public Inventory() {
+    }
+
+    public Inventory(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
