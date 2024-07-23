173da9aad55a70b632d1fae9f73844744ba6539c
Shashinda2001 <142221873+Shashinda2001@users.noreply.github.com>
2024-07-23 15:52:27 +0530

    create JWT service class

---
+++ src/main/java/com/example/Ecommerce_backend/service/JWTservice.java
@@ -0,0 +1,40 @@
+package com.example.Ecommerce_backend.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.example.Ecommerce_backend.model.LocalUser;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class JWTservice{
+
+    @Value("${jwt.algorithm.key}")
+    private String algorithmKey;
+
+    @Value("${jwt.issuer}")
+    private String issuer;
+
+    @Value("${jwt.expiryInSeconds}")
+    private Long expiryInSeconds;
+
+    private Algorithm algorithm;
+
+    private static final String USERNAME_KEY = "USERNAME";
+
+    @PostConstruct
+    public void postConstruct() {
+        algorithm = Algorithm.HMAC256(algorithmKey);
+    }
+
+    public String generateJWT(LocalUser user) {
+        return JWT.create()
+                .withClaim(USERNAME_KEY, user.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + (1000 * expiryInSeconds)))
+                .withIssuer(issuer)
+                .sign(algorithm);
+    }
+}
